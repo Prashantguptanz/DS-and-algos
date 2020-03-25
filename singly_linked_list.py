f57878f6e8a4282d8a9a122193345ca20bf55166
Prashant Gupta <prashantgupta.nz@gmail.com>
2020-03-25 21:28:31 +0530

    refactorized

--- singly_linked_list.py
+++ singly_linked_list.py
@@ -2,104 +2,99 @@
 import sys
 
 class Node:
-	def __init__(self, value):
-		self.value = value
+	def __init__(self, data):
+		self.data = data
 		self.next = None
 
 
 class SinglyLinkedList:
 	def __init__(self):
 		self.head = None
+		self.size = 0
 
+	# Empty the linked list, O(1)
+	def clear(self):
+		self.head = None
+		self.size = 0
 
-	# Insert a node at the front of the list
-	def insert_at_front(self, value):
-		node = Node(value)
-		if self.head is None:
-			self.head = node
-		else:
+	# Return the size of the linked list, O(1)
+	def size(self):
+		return self.size
+
+	# Return if the linked list is empty, O(1)
+	def is_empty(self):
+		return self.size()==0
+
+	# Insert a node at the front of the list, O(1)
+	def insert_at_front(self, data):
+		node = Node(data)
+		if not self.head is None:
 			node.next = self.head
-			self.head = node
+		self.head = node
+		self.size += 1
 
-
-	# Insert a node at the end of the list
-	def insert_at_end(self, value):
-		node = Node(value)
+	# Insert a node at the end of the list, O(n)
+	def insert_at_end(self, data):
+		node = Node(data)
 		if self.head is None:
 			self.head = node
 		else:
 			curr = self.head
-			while curr.next is not None:
+			while not curr.next is None:
 				curr = curr.next
 			curr.next = node
+		self.size += 1
 
-
-	# Insert a node after the given node in the list
-	def insert_after(self, key_node, value):
-		node = Node(value)
+	# Insert an element at the given index in the linked list, O(n)
+	def insert_at(self, index, data):
+		assert 0 <= index < self.size, "Index out of range"
+		node = Node(data)
 		curr = self.head
-		while curr is not None and curr is not key_node:
+		curr_index = 0
+		while curr_index != index-1:
 			curr = curr.next
+			curr_index += 1
 
-		if curr is None:
-			Print("The key node is not found")
-		elif curr.next is None:
-			curr.next = node
-		else:
-			next_node = curr.next
-			curr.next = node
-			node.next = next_node
+		node.next = curr.next
+		curr.next = node
+		self.size += 1
 
-
-	# Remove the node at the front of the linked list
+	# Remove the node at the front of the linked list, O(1)
 	def pop_front_node(self):
-		if self.head is None:
-			print ("List is empty")
-			return
+		assert self.head is not None, "List is empty"
+		head, self.head = self.head, self.head.next
+		data = head.data
+		head = None
+		self.size -= 1
+		return data
 
-		head = self.head
-		self.head = head.next
-		head.value = None
-		head.next = None
-
-	#Remove the node at the end of the linked list
+	#Remove the node at the end of the linked list, O(n)
 	def pop_tail_node(self):
-		curr = self.head
-		if curr is None:
-			Print ("Empty list")
-			return
+		assert self.head is not None, "List is empty"
+		prev, curr = None, self.head
 
-		prev = None
-
-		while curr.next is not None:
+		while not curr.next is None:
 			prev = curr
 			curr = curr.next
 
-		if prev == None:
-			self.head = None
-			curr.value = None
+		if curr is self.head:
+			self.head = curr = None
 		else:
-			prev.next = None
-			curr.value = None
+			prev.next = curr = None
+		self.size -= 1
 
-	# Remove the given node from the linked list
+	# Remove the given node from the linked list, O(n)
 	def remove(self, node):
-		curr = self.head
-		if curr is None:
-			print ("Empty list")
-			return
-
-		prev = None
+		assert self.head is not None, "List is empty"
+		prev, curr = None, self.head
 
 		while curr is not None and curr is not node:
 			prev = curr
 			curr = curr.next
 
-		if curr is None:
-			print ("Node not found")
-		elif prev is None:
-			self.head = self.head.next
-			curr = None
+		assert curr is not None, "Node not found"
+		if prev is None:
+			self.head = curr = None
 		else:
 			prev.next = curr.next
 			curr = None
@@ -107,10 +102,7 @@ class SinglyLinkedList:
 
 	#Check if a node is in the list
 	def check_node(self, node):
-		if self.head is None:
-			Print ("Empty list")
-			return
-
+		assert self.head is not None, "List is empty"
 		curr = self.head
 
 		while curr is not None and curr is not node:
@@ -118,19 +110,16 @@ class SinglyLinkedList:
 
 		if curr is None:
 			return False
-
 		return True
 
 	# print all the items
 	def printlist(self):
-		if self.head is None:
-			print ("Nothing to display")
-		else:
-			curr = self.head
-			while curr != None:
-				sys.stdout.write(str(curr.value) + ' ')
-				curr = curr.next
-			print('\n')
+		assert self.head is not None, "List is empty"
+		curr = self.head
+		while curr != None:
+			sys.stdout.write(str(curr.data) + ' ')
+			curr = curr.next
+		print('\n')
 
 
 if __name__ == '__main__':
@@ -139,7 +128,8 @@ if __name__ == '__main__':
     list.insert_at_front(3)
     list.insert_at_front(2)
     list.insert_at_end(10)
-    list.insert_after(list.head, 7)
+    list.insert_at(2, 7)
+    list.pop_front_node()
     list.remove(list.head.next)
     print(list.check_node(list.head.next))
     list.printlist()
